f5b53cadbce2d0e9420b402e20bb97c5f81707d5
raghubk4589 <raghubk4589@gmail.com>
2018-07-17 16:32:23 +0530

    updated deploy file

--- deploy.php
+++ deploy.php
@@ -4,7 +4,7 @@
 	 *
 	 * Used for automatically deploying websites via github or bitbucket, more deets here:
 	 * https://gist.github.com/riodw/71f6e2244534deae652962b32b7454e2
-	 * How To Use:
+	 * How To Use:  
 	 * https://medium.com/riow/deploy-to-production-server-with-git-using-php-ab69b13f78ad
 	 */
 	// The commands
